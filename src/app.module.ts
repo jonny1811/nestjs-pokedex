872d77c8f6d3c1de25f93759aa76d5ab7b5432a8
Jonny Lezcano <jlezcanoa@ext.falabella.cl>
2023-09-08 17:24:39 -0400

    feat: add seed module to recreate DB data

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,20 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
-    imports: [
-        ServeStaticModule.forRoot({
-            rootPath: join(__dirname, '..', 'public'),
-        }),
-        MongooseModule.forRoot(
-            'mongodb://root:password@localhost:27017/?authMechanism=DEFAULT',
-        ),
-        PokemonModule,
-        CommonModule,
-    ],
+	imports: [
+		ServeStaticModule.forRoot({
+			rootPath: join(__dirname, '..', 'public'),
+		}),
+		MongooseModule.forRoot(
+			'mongodb://root:password@localhost:27017/?authMechanism=DEFAULT',
+			{ dbName: "pokemon" }
+		),
+		PokemonModule,
+		CommonModule,
+		SeedModule,
+	],
 })
-export class AppModule {}
+export class AppModule { }
